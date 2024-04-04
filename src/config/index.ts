af5f2f4349bf4c727c2f276366349c08aefa2982
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-04-03 22:10:43 -0400

    Update buyPercent and minLiquidityLockedUSD values

--- src/config/index.ts
+++ src/config/index.ts
@@ -29,13 +29,13 @@ function loadConfig(): Config {
     telegramBot: validateEnvVariable("TEL_BOT_TOKEN"),
     walletFilePassword: validateEnvVariable("WALLET_FILE_PASSWORD"),
 
-    buyPercent: 80,
+    buyPercent: 60,
     loopSeconds: 10000 /* 10 seconds */,
     maxProfit: 10 /* x10 */,
     timeToCheckMaxProfit: 1000 * 2 /* 2 seconds */,
     timeForFirstSell: 1000 * 60 /* 1 minute */,
     timeForSecondSell: 1000 * 60 * 3 /* 3 minutes */,
-    minLiquidityLockedUSD: 800 /* 800 usd  */,
+    minLiquidityLockedUSD: 500 /*  usd  */,
     telegramChatIds: [709820730],
   };
 }
